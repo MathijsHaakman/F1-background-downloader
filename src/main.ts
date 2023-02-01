5cc106930969ddebdbf49dfb17e056b2dcb62447
Mathijs Haakman <mathijshaakman@outlook.com>
2023-02-01 23:32:21 +0100

    Retrieve highest definition URL

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import probe from 'probe-image-size';
 
-let url = 'https://pbs.twimg.com/media/FMceCAbXIAItvrJ?format=jpg&name=large';
+const prepareUrl = (url: string): string => {
+  const parsedUrl = new URL(url);
+  parsedUrl.searchParams.set('name', 'orig');
 
-probe(url).then(result => {
+  return parsedUrl.toString();  
+};
+
+const highDefUrl = prepareUrl('https://pbs.twimg.com/media/FMceCAbXIAItvrJ?format=jpg&name=large');
+
+probe(highDefUrl).then(result => {
   console.log(result);
 });
